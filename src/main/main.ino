2ae5b6abae7f093caab3ae293c36f4d70f84937b
Tyler Martin <tdmart05@gmail.com>
2017-07-13 12:51:32 -0400

    Room 1 Working, when reset is pushed timers reset but continue to count.

--- src/main/main.ino
+++ src/main/main.ino
@@ -8,29 +8,34 @@
 
 #include <CountUpDownTimer.h>
 
-#include "Adafruit_LEDBackpack.h" // For Control Panel Displays
+#include <Adafruit_LEDBackpack.h> // For Control Panel Displays
 #include <LedControl.h>           // For Shower Room Displays
 #include <LiquidCrystal.h>
 
-const float version  = 0.4;
+const float version  = 0.7;
 
 // Define Inputs
 const int PBPins[] = {
-                       22,23,24,25, //Timer 1 Inc,Dec,Sel,Save
-                       26,27,28,29, //Timer 2 Inc,Dec,Sel,Save
-                       0,0,     //Start Room 1, Start Room 2,
-                       0,0,     //Start Shower 1, Start Shower 2,
+                       26,27,28,29, //Timer 1 Inc,Dec,Reset,XX
+                       22,23,24,25, //Timer 2 Inc,Dec,Reset,XX
+                       34,24,     //Start Room 1, Start Room 2,
+                       30,25,     //Start Shower 1, Start Shower 2,
 };
 const int PBcount = 12;
+
 // Define Outputs
-const int ColdWater1Relay = 2;
+
+const int ColdWater1Relay = 4;
 const int HotWater1Relay  = 3;
-const int ColdWater2Relay = 4;
+const int ColdWater2Relay = 2;
 const int HotWater2Relay  = 5;
 
+const int Room1Buzzer     = 36;
+const int Room2Buzzer     = 7;
+
 // Define Relay State
-const int RelayClosed = HIGH;
-const int RelayOpen   = LOW;
+const int WaterFlowing    = HIGH;
+const int WaterBlocked    = LOW;
 
 // Define displays
 Adafruit_7segment Room1TimerCPDis = Adafruit_7segment();
@@ -62,39 +67,51 @@ CountUpDownTimer Water2Timer(DOWN);
 CountUpDownTimer Room1OverTimer(UP);
 CountUpDownTimer Room2OverTimer(UP);
 
+// Default Times
+int RoomTimeMinutes   = 15;
+int ShowerTimeMinutes = 7;
+
 //Define Button Debounce
 const int debounceDelay   = 250; //milliseconds
 const int debounceWindow = 100; //milliseconds
 
 long PBTimes[] = {
-                       0,0,0,0, //Timer 1 Inc,Dec,Sel,Save
-                       0,0,0,0, //Timer 2 Inc,Dec,Sel,Save
+                       0,0,0,0, //Timer 1 Inc,Dec,Reset,XX
+                       0,0,0,0, //Timer 2 Inc,Dec,Reset,XX
                        0,0,     //Start Room 1, Start Room 2,
                        0,0      //Start Shower 1, Start Shower 2,
 };
 
 int PBLastStates[] = {
-                       0,0,0,0, //Timer 1 Inc,Dec,Sel,Save
-                       0,0,0,0, //Timer 2 Inc,Dec,Sel,Save
+                       0,0,0,0, //Timer 1 Inc,Dec,Reset,XX
+                       0,0,0,0, //Timer 2 Inc,Dec,Reset,XX
                        0,0,     //Start Room 1, Start Room 2,
                        0,0      //Start Shower 1, Start Shower 2,
 };
 
 int PBStates[] = {
-                       LOW,LOW,LOW,LOW, //Timer 1 Inc,Dec,Sel,Save
-                       LOW,LOW,LOW,LOW, //Timer 2 Inc,Dec,Sel,Save
+                       LOW,LOW,LOW,LOW, //Timer 1 Inc,Dec,Reset,XX
+                       LOW,LOW,LOW,LOW, //Timer 2 Inc,Dec,Reset,XX
                        LOW,LOW,         //Start Room 1, Start Room 2,
                        LOW,LOW          //Start Shower 1, Start Shower 2,
 };
 
-boolean PBOneShots[] = {
-                       false,false,false,false, //Timer 1 Inc,Dec,Sel,Save
-                       false,false,false,false, //Timer 2 Inc,Dec,Sel,Save
+bool PBOneShots[] = {
+                       false,false,false,false, //Timer 1 Inc,Dec,Reset,XX
+                       false,false,false,false, //Timer 2 Inc,Dec,Reset,XX
                        false,false,             //Start Room 1, Start Room 2,
                        false,false              //Start Shower 1, Start Shower 2,
 };
 
-boolean ForceTimerUpdate    = false;
+
+//define flags
+bool ForceTimerUpdate    = false;
+
+bool Room1TimerRunning   = false;
+bool Room2TimerRunning   = false;
+
+bool Shower1HasNotRun    = true;
+bool Shower2HasNotRun    = true;
 
 void setup()
 {
@@ -112,17 +129,11 @@ void setup()
   Water2TimerCPDis.begin(0x71); //Set address for water timer 2 control panel display
 
   //Init Timers
-  Room1Timer.SetTimer(0,45,0);
-  Room1Timer.StartTimer();
-  
-  Water1Timer.SetTimer(0,11,0);
-  Water1Timer.StartTimer();
+  Room1Timer.SetTimer(0,RoomTimeMinutes,0); 
+  Water1Timer.SetTimer(0,ShowerTimeMinutes,0);
 
-  Room2Timer.SetTimer(0,15,0);
-  Room2Timer.StartTimer();
-  
-  Water2Timer.SetTimer(0,5,0);
-  Water2Timer.StartTimer();
+  Room2Timer.SetTimer(0,RoomTimeMinutes,0);
+  Water2Timer.SetTimer(0,ShowerTimeMinutes,0);
 
   Room1OverTimer.SetTimer(0,0,0);
   Room2OverTimer.SetTimer(0,0,0);
@@ -138,12 +149,17 @@ void setup()
   pinMode(ColdWater2Relay, OUTPUT);
   pinMode(HotWater2Relay, OUTPUT);
 
-  //Set Outputs
-  digitalWrite(ColdWater1Relay, RelayOpen);
-  digitalWrite(HotWater1Relay, RelayOpen);
-  digitalWrite(ColdWater2Relay, RelayOpen);
-  digitalWrite(HotWater2Relay, RelayOpen);
+  pinMode(Room1Buzzer, OUTPUT);
+  pinMode(Room2Buzzer, OUTPUT);
 
+  //Set Outputs
+  digitalWrite(ColdWater1Relay, WaterBlocked);
+  digitalWrite(HotWater1Relay, WaterBlocked);
+  digitalWrite(ColdWater2Relay, WaterBlocked);
+  digitalWrite(HotWater2Relay, WaterBlocked);
+
+  digitalWrite(Room1Buzzer, LOW);
+  digitalWrite(Room2Buzzer, LOW);
   /*
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
@@ -158,9 +174,20 @@ void setup()
   Room1TimerDis.clearDisplay(1);
 
   Room1TimerDis.setScanLimit(1,5);
- // Room2TimerDis.setIntensity(1,15);
+
+  Room2TimerDis.shutdown(0,false);
+  Room2TimerDis.shutdown(1,false);
+  /* Set the brightness to a medium values */
+  Room2TimerDis.setIntensity(0,15);
+  Room2TimerDis.setIntensity(1,15);
+  /* and clear the display */
+  Room2TimerDis.clearDisplay(0);
+  Room2TimerDis.clearDisplay(1);
+
+  Room2TimerDis.setScanLimit(1,5);
 
  delay(1500);
+ ForceTimerUpdate = true;
 }
 
 
@@ -208,17 +235,17 @@ void loop() {
   if ((PBStates[0] == HIGH) && (PBOneShots[0] == true))
   {
     PBOneShots[0] = false;
-    Serial.println("INC Pressed");
-    if(Room1Timer.ShowMinutes() <= 58)
+    Serial.println("INC 1 Pressed");
+    if(Water1Timer.ShowMinutes() <= 58)
     {
-      Room1Timer.SetTimer(0,Room1Timer.ShowMinutes()+1,Room1Timer.ShowSeconds()); 
+      Water1Timer.SetTimer(0,Water1Timer.ShowMinutes()+1,Water1Timer.ShowSeconds()); 
       ForceTimerUpdate = true;
     }
   }
   else if (PBStates[0] == LOW && PBOneShots[0] == true)
   {
     PBOneShots[0] = false;
-    Serial.println("INC Released");
+    Serial.println("INC 1 Released");
   }
 
   //Array Pos 1
@@ -226,18 +253,179 @@ void loop() {
   if ((PBStates[1] == HIGH) && (PBOneShots[1] == true))
   {
     PBOneShots[1] = false;
-    Serial.println("DEC Pressed");
-    if(Room1Timer.ShowMinutes() > 0)
+    Serial.println("DEC 1 Pressed");
+    if(Water1Timer.ShowMinutes() > 0)
     {
-      Room1Timer.SetTimer(0,Room1Timer.ShowMinutes()-1,Room1Timer.ShowSeconds()); 
+      Water1Timer.SetTimer(0,Water1Timer.ShowMinutes()-1,Water1Timer.ShowSeconds()); 
       ForceTimerUpdate = true;
     }
   }
   else if (PBStates[1] == LOW && PBOneShots[1] == true)
   {
     PBOneShots[1] = false;
-    Serial.println("DEC Released");
+    Serial.println("DEC 1 Released");
   }
+
+  //Array Pos 2
+  //Timer 1 XX
+  
+  
+  //Array Pos 3
+  //Timer 1 Reset
+  if ((PBStates[3] == HIGH) && (PBOneShots[3] == true))
+  {
+    PBOneShots[3] = false;
+    Serial.println("Reset 1 Pressed");
+    Room1Timer.StopTimer();
+    Water1Timer.StopTimer();
+    Room1Timer.SetTimer(0,RoomTimeMinutes,0); 
+    Water1Timer.SetTimer(0,ShowerTimeMinutes,0);
+    Room1TimerRunning = false;
+    Shower1HasNotRun = true;
+    digitalWrite(Room1Buzzer, LOW);
+    digitalWrite(ColdWater1Relay, WaterBlocked);
+    digitalWrite(HotWater1Relay, WaterBlocked);
+    
+  }
+  else if (PBStates[3] == LOW && PBOneShots[3] == true)
+  {
+    PBOneShots[3] = false;
+    Serial.println("Reset 1 Released");
+  }
+  //Array Pos 4
+  //Timer 2 Increment
+  if ((PBStates[4] == HIGH) && (PBOneShots[4] == true))
+  {
+    PBOneShots[4] = false;
+    Serial.println("INC 2 Pressed");
+    if(Room2Timer.ShowMinutes() <= 58)
+    {
+      Room2Timer.SetTimer(0,Room2Timer.ShowMinutes()+1,Room2Timer.ShowSeconds()); 
+      ForceTimerUpdate = true;
+    }
+  }
+  else if (PBStates[4] == LOW && PBOneShots[4] == true)
+  {
+    PBOneShots[4] = false;
+    Serial.println("INC 2 Released");
+  }
+  
+  //Array Pos 5
+  //Timer 1 Decrement
+  if ((PBStates[5] == HIGH) && (PBOneShots[5] == true))
+  {
+    PBOneShots[5] = false;
+    Serial.println("DEC 2 Pressed");
+    if(Room2Timer.ShowMinutes() > 0)
+    {
+      Room2Timer.SetTimer(0,Room2Timer.ShowMinutes()-1,Room2Timer.ShowSeconds()); 
+      ForceTimerUpdate = true;
+    }
+  }
+  else if (PBStates[5] == LOW && PBOneShots[5] == true)
+  {
+    PBOneShots[5] = false;
+    Serial.println("DEC 2 Released");
+  }
+  
+  //Array Pos 6
+  //Room 2 Reset
+  if ((PBStates[6] == HIGH) && (PBOneShots[6] == true))
+  {
+    PBOneShots[6] = false;
+    Serial.println("Reset 2 Pressed");
+    Room2Timer.StopTimer();
+    Water2Timer.StopTimer();
+    Room2Timer.SetTimer(0,RoomTimeMinutes,0); 
+    Water2Timer.SetTimer(0,ShowerTimeMinutes,0);
+    Room2TimerRunning = false;
+    Shower2HasNotRun = true;
+    digitalWrite(Room2Buzzer, LOW);
+    digitalWrite(ColdWater2Relay, WaterBlocked);
+    digitalWrite(HotWater2Relay, WaterBlocked);
+  }
+  else if (PBStates[6] == LOW && PBOneShots[6] == true)
+  {
+    PBOneShots[6] = false;
+    Serial.println("Reset 2 Released");
+  }
+  
+  //Array Pos 7
+  //Room 2 XX
+  
+  //Array Pos 8
+  //Start Room 1 Timer
+  if ((PBStates[8] == HIGH) && (PBOneShots[8] == true))
+  {
+    PBOneShots[8] = false;
+    Serial.println("Room 1 Pressed");
+    Room1Timer.StartTimer();
+    Room1TimerRunning = true;
+  }
+  else if (PBStates[8] == LOW && PBOneShots[8] == true)
+  {
+    PBOneShots[8] = false;
+    Serial.println("Room 1 Released");
+  }
+  
+  //Array Pos 9
+  //Start Room 2 Timer
+  if ((PBStates[9] == HIGH) && (PBOneShots[9] == true))
+  {
+    PBOneShots[9] = false;
+    Serial.println("Room 2 Pressed");
+    Room2Timer.StartTimer();
+    Room2TimerRunning = true;
+  }
+  else if (PBStates[9] == LOW && PBOneShots[9] == true)
+  {
+    PBOneShots[9] = false;
+    Serial.println("Room 2 Released");
+  }
+  
+  //Array Pos 10
+  //Start Room 1 Shower Timer
+  if ((PBStates[10] == HIGH) && (PBOneShots[10] == true))
+  {
+    PBOneShots[10] = false;
+    Serial.println("Shower 1 Pressed");
+    if(Room1TimerRunning && Shower1HasNotRun)
+    {
+      Serial.println("Shower 1 Timer Started");
+      Water1Timer.StartTimer();
+      digitalWrite(ColdWater1Relay, WaterFlowing);
+      digitalWrite(HotWater1Relay, WaterFlowing);
+      Shower1HasNotRun = false; 
+    }
+    
+  }
+  else if (PBStates[10] == LOW && PBOneShots[10] == true)
+  {
+    PBOneShots[10] = false;
+    Serial.println("Shower 1 Released");
+  }
+
+  //Array Pos 11
+  //Start Room 2 Shower Timer
+  if ((PBStates[11] == HIGH) && (PBOneShots[11] == true))
+  {
+    PBOneShots[11] = false;
+    Serial.println("Shower 2 Pressed");
+    if(Room2TimerRunning && Shower1HasNotRun)
+    {
+      Serial.println("Shower 2 Timer Started");
+      Water2Timer.StartTimer();
+      digitalWrite(ColdWater2Relay, WaterFlowing);
+      digitalWrite(HotWater2Relay, WaterFlowing); 
+      Shower2HasNotRun = false;
+    }
+  }
+  else if (PBStates[11] == LOW && PBOneShots[11] == true)
+  {
+    PBOneShots[11] = false;
+    Serial.println("Shower 2 Released");
+  }
+
 
   // Update timer information
   Room1Timer.Timer();
@@ -331,6 +519,182 @@ void loop() {
     Water2TimerCPDis.writeDisplay();
   }
 
+  //Check to see if buzzer needs to fire
+  
+//Room 1
+  if (Water1Timer.TimeCheck(0,1,0))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,59))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+
+  if (Water1Timer.TimeCheck(0,0,50))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,49))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+
+  if (Water1Timer.TimeCheck(0,0,40))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,39))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+
+  if (Water1Timer.TimeCheck(0,0,30))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,29))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+
+  if (Water1Timer.TimeCheck(0,0,20))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,19))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+  
+  if (Water1Timer.TimeCheck(0,0,10))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,9))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+
+  if (Water1Timer.TimeCheck(0,0,2))
+  {
+    digitalWrite(Room1Buzzer, HIGH);
+  }
+
+  if (Water1Timer.TimeCheck(0,0,0))
+  {
+   digitalWrite(Room1Buzzer, LOW); 
+  }
+  
+  if (Water1Timer.TimeCheck(0,0,0))
+  {
+   digitalWrite(ColdWater1Relay, WaterBlocked);
+   digitalWrite(HotWater1Relay, WaterBlocked); 
+  }
+  
+//Room 2
+  if (Water2Timer.TimeCheck(0,1,0))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,59))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,50))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,49))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,40))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,39))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,30))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,29))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,20))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,19))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,10))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,9))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+
+  if (Water2Timer.TimeCheck(0,0,2))
+  {
+    digitalWrite(Room2Buzzer, HIGH);
+  }
+
+  if (Water2Timer.TimeCheck(0,0,0))
+  {
+   digitalWrite(Room2Buzzer, LOW); 
+  }
+  
+  if (Water2Timer.TimeCheck(0,0,0))
+  {
+   digitalWrite(ColdWater2Relay, WaterBlocked);
+   digitalWrite(HotWater2Relay, WaterBlocked); 
+  }
+
+//Check to see if Shower timers have started
+
+  if(Room1Timer.TimeCheck(0,ShowerTimeMinutes,1) && Shower1HasNotRun)
+  {
+    Serial.println("Shower 1 Timer Started");
+    Water1Timer.StartTimer();
+    digitalWrite(ColdWater1Relay, WaterFlowing);
+    digitalWrite(HotWater1Relay, WaterFlowing);
+    Shower1HasNotRun = false;
+  }
+
+  if(Room2Timer.TimeCheck(0,ShowerTimeMinutes,1) && Shower2HasNotRun)
+  {
+    Serial.println("Shower 2 Timer Started");
+    Water2Timer.StartTimer();
+    digitalWrite(ColdWater2Relay, WaterFlowing);
+    digitalWrite(HotWater2Relay, WaterFlowing);
+    Shower2HasNotRun = false;
+  }
+  
   ForceTimerUpdate = false;
   
 }//END MAIN
